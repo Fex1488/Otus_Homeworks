cd6edd196cfd4c91274dce499a2df461b4d25906
Sergei Kachaev <88f@mail.ru>
2022-04-02 20:30:38 +0300

    figure

---
+++ src/Circle.py
@@ -0,0 +1,1 @@
+import mathfrom src.Figure import Figureclass Circle(Figure):    def __init__(self, r):        self.__r = r        self.__area = int(math.pi * (r ** 2))        self.__perimeter = int(2 * math.pi * r)        super().__init__('Круг', self.__area, self.__perimeter)
